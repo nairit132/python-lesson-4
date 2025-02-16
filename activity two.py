2ce2f72ce872596f0590173e89cf3d61a2dee201
nairit132 <nairithmajmundar@gmail.com>
2025-02-16 17:52:55 +0000

    Add files via upload

---
+++ activity two.py
@@ -0,0 +1,8 @@
+maths = int(input('maths'))
+english = int(input('english'))
+science = int(input('science'))
+spanish = int(input('spanish'))
+avrage_score = (maths+english+science+spanish)/4
+print ("Your mean for the score was ", avrage_score)
+percentage = avrage_score
+print ("The percentage was ", percentage,"%")
